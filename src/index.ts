d123fe0ae3ae621c02672361928d253d6d3f8be8
Rubens Armôa Lopes <github@rubenslop.es>
2022-12-16 22:49:18 -0400

    ajusta index macarronico

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import './estilos.scss'
+
 document.addEventListener(`DOMContentLoaded`, () => {
   const elementoTítulo = document.querySelector(`h1`)!
 
-import './estilos.scss'
-
+  const entradaTítulo = document.querySelector<HTMLInputElement>(`[name="título"]`)!
+  const entradaSubtítulo = document.querySelector<HTMLInputElement>(`[name="subtítulo"]`)!
+  
   entradaTítulo.addEventListener(`keyup`, () => {
     elementoTítulo.innerHTML = `
       ${entradaTítulo.value}
